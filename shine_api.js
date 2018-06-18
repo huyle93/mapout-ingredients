9a9ff4100ffd2e44c8896df7511c721a38403ef7
Jason Bortolussi <jcb1023@wildcats.unh.edu>
2018-06-18 12:40:50 -0400

    modified other two shine_api functions

--- shine_api.js
+++ shine_api.js
@@ -4,54 +4,39 @@ var https = require('https');
 //Shine API car Theft
 let request = require('request')
 var shine_key = process.env.shineapi_KEY
-let options = {
-  "url": "https://apis.solarialabs.com/shine/v1/vehicle-thefts?state=nh&rank=3&apikey=" + shine_key,
-  "method": "GET",
-  "qs": {
-    "state": "nh",
-    "rank": "3",
-    "apikey": shine_key
+
+function httpsGetTheftStats( stateCode, callback){
+  var theft_options = {
+    host: 'apis.solarialabs.com',
+    path: '/shine/v1/vehicle-thefts?state=' + stateCode + '&apikey=' + shine_key,
+    method: 'GET'
   }
+
+    var req = https.request(theft_options, function(res) {
+    res.setEncoding('utf-8');
+
+    var responseString = '';
+
+    res.on('data', function(data) {
+        responseString += data;
+    });
+
+    res.on('end', function() {
+        var response = JSON.parse(responseString);
+        var theft_make = response[0].Make
+        var theft_model = response[0].Model
+        var theft_state = response[0].State
+
+        console.log( "The #1 most stolen car in " + theft_state + " is the "+ theft_make + " " + theft_model + ".");
+        callback([theft_make, theft_model]);
+    });
+    });
+
+    req.end();
 }
-request(options,(err,resp,body)=>{
-var data = JSON.parse(body)
-
-var theft_code = data[0].Code;
-var theft_rank = data[0].Rank;
-var theft_make = data[0].Make;
-var theft_model = data[0].Model;
-var theft_year = data[0].Year;
-var theft_num_thefts = data[0].Thefts;
-
-})
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Shine API car stat
-/*
-let stat = {
-  "url": "https://apis.solarialabs.com/shine/v1/vehicle-stats/specs?make=honda&model=cr-v&year=2010&full-data=true&apikey=" + shine_key,
-  "method": "GET",
-  "qs": {
-    "make": "toyota",
-    "model": "venza",
-    "year": "2010",
-    "full-data": "true",
-    "apikey": shine_key
-    }
-}
-request(stat,(err,resp,body)=>{
-var data = JSON.parse(body)
-
-var stats_make = data[0].Make;
-var stats_model = data[0].Model;
-var stats_car_year = data[0].Model_Year;
-var stats_car_mpg = data[0].Comb_Conventional_Fuel;
-
-
-//console.log( "Model Year of the " + stats_make + " " + stats_model + " is: " + stats_car_year + ". The combined highway and city MPG is " + stats_car_mpg + ".");
-*/
-
-//variables for testing
 function httpsGetStats(make, model, year, callback){
   var stats_options = {
     host: 'apis.solarialabs.com',
@@ -83,26 +68,63 @@ function httpsGetStats(make, model, year, callback){
   req.end();
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Shine API prediction call
+function httpsGetPredictionStats(best_or_worst, callback){
+  var predictions_options = {
+    host: 'apis.solarialabs.com',
+    path: '/shine/v1/vehicle-stats/five-year-costs?top-ten=' + best_or_worst + '&apikey=' + shine_key,
+    method: 'GET'
+  }
+
+  var req = https.request(predictions_options, function(res) {
+  res.setEncoding('utf-8');
+
+  var responseString = '';
+
+  res.on('data', function(data) {
+      responseString += data;
+  });
+
+  res.on('end', function() {
+      var response = JSON.parse(responseString);
+      var predict_manufacturer = response[0].Manufacturer
+      var predict_model = response[0].Model
+
+      console.log( 'The predicted ' + best_or_worst + ' model of car based on the increased amount spent over 5 years is the ' + predict_manufacturer + ' ' + predict_model + '.' );
+      callback([predict_manufacturer, predict_model]);
+  });
+  });
+
+  req.end();
+}
+
 httpsGetStats( "toyota", "camry", "2011", (stats) => {
   var year = stats[0];
   var mpg = stats[1];
 })
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Shine API prediction call
-let prediction = {
-  "url": "https://apis.solarialabs.com/shine/v1/vehicle-stats/five-year-costs?top-ten=worst&apikey=" + shine_key,
-  "method": "GET",
-  "qs": {
-    "top-ten": "worst",
-    "apikey": shine_key
-  }
-}
-request(prediction,(err,resp,body)=>{
-var data = JSON.parse(body)
-for(var i = 0; i < 10; i++){
-  //Uncomment this line to test
- //console.log(data[i].Model, data[i].Model_Year, data[i].Manufacturer, (data[i].City_Conventional_Fuel + data[i].Hwy_Conventional_Fuel)/2,
-  //data[i].Money_You_Spend_over_5_years_increased_amount_spent_in_fuel_costs_over_5_years_on_label)
-}
+httpsGetTheftStats( "nh", (theft_car) => {
+  var theft_car_make = theft_car[0];
+  var theft_car_model = theft_car[1];
+})
+
+httpsGetTheftStats( "ak", (theft_car) => {
+  var theft_car_make = theft_car[0];
+  var theft_car_model = theft_car[1];
+})
+
+httpsGetTheftStats( "ca", (theft_car) => {
+  var theft_car_make = theft_car[0];
+  var theft_car_model = theft_car[1];
+})
+
+httpsGetPredictionStats( "best", (prediction_car) => {
+  var prediction_car_make = prediction_car[0];
+  var prediction_car_model = prediction_car[1];
+})
+
+httpsGetPredictionStats( "worst", (prediction_car) => {
+  var prediction_car_make = prediction_car[0];
+  var prediction_car_model = prediction_car[1];
 })
